e8825b3303dca7a220ca73913eac4faa09adbd9d
reyreavman <radmirkhurum@gmail.com>
2024-07-05 11:58:34 +0700

    - в MultipleParamsErrorResponse добавлен статус, дата/время возникновения ошибки - в ValidationErrorResponse также добавлен статус, дата/время возникновения ошибки

--- src/main/java/ru/cft/template/core/exception/responseWrapper/MultipleParamsErrorResponse.java
+++ src/main/java/ru/cft/template/core/exception/responseWrapper/MultipleParamsErrorResponse.java
@@ -2,18 +2,17 @@ package ru.cft.template.core.exception.responseWrapper;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import ru.cft.template.core.exception.common.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class MultipleParamsErrorResponse {
+    private final HttpStatus status;
     private final String message;
     private final List<Param> multipleParamsList;
-
-    public MultipleParamsErrorResponse(String messageParam, Param... param) {
-        this.message = messageParam;
-        this.multipleParamsList = List.of(param);
-    }
+    private final LocalDateTime dateTime = LocalDateTime.now();
 }
--- src/main/java/ru/cft/template/core/exception/responseWrapper/ValidationErrorResponse.java
+++ src/main/java/ru/cft/template/core/exception/responseWrapper/ValidationErrorResponse.java
@@ -2,16 +2,21 @@ package ru.cft.template.core.exception.responseWrapper;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import ru.cft.template.core.exception.common.Violation;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class ValidationErrorResponse {
+    private final HttpStatus status;
     private final List<Violation> violationList;
+    private final LocalDateTime dateTime = LocalDateTime.now();
 
-    public ValidationErrorResponse(Violation... violation) {
+    public ValidationErrorResponse(HttpStatus status, Violation... violation) {
+        this.status = status;
         this.violationList = List.of(violation);
     }
 }
