5a272cc21c8f1a57605b132a7a881ac69ead7b69
reyreavman <radmirkhurum@gmail.com>
2024-07-07 13:17:31 +0700

    - рефакторинг wallet сервиса в соответствии с новым hesoyamGenerator

--- src/main/java/ru/cft/template/core/service/wallet/DefaultWalletService.java
+++ src/main/java/ru/cft/template/core/service/wallet/DefaultWalletService.java
@@ -3,14 +3,15 @@ package ru.cft.template.core.service.wallet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.cft.template.api.dto.WalletDTO;
 import ru.cft.template.api.dto.WalletHesoyamDTO;
-import ru.cft.template.common.HesoyamRouletteGenerator;
 import ru.cft.template.core.entity.User;
 import ru.cft.template.core.entity.Wallet;
 import ru.cft.template.core.exception.EntityNotFoundException;
 import ru.cft.template.core.repository.UserRepository;
 import ru.cft.template.core.repository.WalletRepository;
+import ru.cft.template.core.utils.HesoyamRouletteGeneratorUtil;
 
 import java.util.Map;
 
@@ -21,20 +22,22 @@ public class DefaultWalletService implements WalletService {
     private final UserRepository userRepository;
 
     private final ConversionService conversionService;
-    private final HesoyamRouletteGenerator hesoyamGenerator;
+    private final HesoyamRouletteGeneratorUtil hesoyamGenerator;
 
     @Override
+    @Transactional
     public WalletDTO findByUserId(Long userId) {
         Wallet wallet = this.walletRepository.findById(userId).orElseGet(() -> {
-            User user = this.userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("userId", String.valueOf(userId))));
+            User user = this.userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("id", String.valueOf(userId))));
             return this.walletRepository.save(new Wallet(null, user, 100L));
         });
         return this.conversionService.convert(wallet, WalletDTO.class);
     }
 
     @Override
+    @Transactional
     public WalletHesoyamDTO hesoyam(Long userId) {
-        Wallet wallet = this.walletRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(Wallet.class.getSimpleName(), Map.of("userId", String.valueOf(userId))));
+        Wallet wallet = this.walletRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(Wallet.class.getSimpleName(), Map.of("id", String.valueOf(userId))));
         if (this.hesoyamGenerator.call()) {
             wallet.setHesoyamWinner(true);
             wallet.setBalance(wallet.getBalance() + wallet.getHesoyamWinningAmount());
