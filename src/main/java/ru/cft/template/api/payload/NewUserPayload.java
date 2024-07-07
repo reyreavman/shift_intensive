898547bca1b97eeb41787f30037909afa6360c2a
reyreavman <radmirkhurum@gmail.com>
2024-07-07 13:08:55 +0700

    - добавил месседжи для валидацию полей на null

--- src/main/java/ru/cft/template/api/payload/NewUserPayload.java
+++ src/main/java/ru/cft/template/api/payload/NewUserPayload.java
@@ -5,9 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 @Builder
 public record NewUserPayload(
@@ -25,20 +22,20 @@ public record NewUserPayload(
         @Pattern(regexp = "^[À-ß¨][à-ÿ¸]{1,50}$", message = "{wallet.users.create.errors.name_condition_is_invalid}")
         String lastName,
 
-        @NotNull
+        @NotNull(message = "{wallet.users.create.errors.phoneNumber_is_null}")
         @Size(min = 11, max = 11, message = "{wallet.users.create.errors.phoneNumber_size_is_invalid}")
         @Pattern(regexp = "^7\\d{10}$", message = "{wallet.users.create.errors.phoneNumber_condition_is_invalid}")
         String phoneNumber,
 
-        @NotNull
+        @NotNull(message = "{wallet.users.create.errors.email_is_null}")
         @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "{wallet.users.create.errors.email_condition_is_invalid}")
         String email,
 
-        @NotNull
+        @NotNull(message = "{wallet.users.create.errors.birthdate_is_null}")
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}$", message = "{wallet.users.create.errors.birthdate_condition_is_invalid}")
         String birthdate,
 
-        @NotNull
+        @NotNull(message = "{wallet.users.create.errors.password_is_null}")
         @Size(min = 8, max = 64, message = "{wallet.users.create.errors.password_size_is_invalid}")
         @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!?])[a-zA-Z0-9!?]{8,64}$", message = "{wallet.users.create.errors.password_condition_is_invalid}")
         String password
