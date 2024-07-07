b1379daa509ab3c18a58f343863e909878a6f2c8
reyreavman <radmirkhurum@gmail.com>
2024-07-07 13:46:50 +0700

    - добавил сервис для трансферов

---
+++ src/main/java/ru/cft/template/core/service/transfer/TransferAmongUsersService.java
@@ -0,0 +1,28 @@
+package ru.cft.template.core.service.transfer;
+
+import jakarta.validation.Valid;
+import ru.cft.template.api.dto.transfer.TransferAmongUsersDataDTO;
+import ru.cft.template.api.dto.transfer.TransferByEmailDTO;
+import ru.cft.template.api.dto.transfer.TransferByPhoneNumberDTO;
+import ru.cft.template.api.payload.transfer.NewTransferByEmailPayload;
+import ru.cft.template.api.payload.transfer.NewTransferByPhoneNumberPayload;
+import ru.cft.template.core.entity.transfer.TransferDirectionType;
+import ru.cft.template.core.entity.transfer.TransferStatus;
+
+import java.util.List;
+
+public interface TransferAmongUsersService {
+    TransferByEmailDTO createTransferToUserByEmail(@Valid NewTransferByEmailPayload transferPayload);
+
+    TransferByPhoneNumberDTO createTransferToUserByPhoneNumber(@Valid NewTransferByPhoneNumberPayload transferPayload);
+
+    List<TransferAmongUsersDataDTO> findAllTransfersByUserId(Long userId);
+
+    TransferAmongUsersDataDTO findTransferById(Long id);
+
+    List<TransferAmongUsersDataDTO> findTransfersByStatus(TransferStatus status);
+
+    List<TransferAmongUsersDataDTO> findTransfersByType(TransferDirectionType type);
+
+    List<TransferAmongUsersDataDTO> findTransfersByTypeAndStatus(TransferDirectionType type, TransferStatus status);
+}
