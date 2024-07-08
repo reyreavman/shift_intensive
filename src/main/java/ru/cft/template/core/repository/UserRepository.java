d4573b9f8d6b0a7a7922df0954cc876f45d1babc
reyreavman <radmirkhurum@gmail.com>
2024-07-09 03:31:44 +0700

    - добавлен метод, возвращающий всех юзеров, обернутых в лист

--- src/main/java/ru/cft/template/core/repository/UserRepository.java
+++ src/main/java/ru/cft/template/core/repository/UserRepository.java
@@ -3,10 +3,13 @@ package ru.cft.template.core.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.cft.template.core.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByPhoneNumber(String phoneNumber);
+
+    List<User> findAll();
 }
