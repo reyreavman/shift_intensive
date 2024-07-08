74d055ec2988da83a66eafbeefb97fefce5da0b5
reyreavman <radmirkhurum@gmail.com>
2024-07-09 03:36:29 +0700

    - рефакторинг сервисов юзера и кошелька, стали гораздо приятнее

--- src/main/java/ru/cft/template/core/service/user/DefaultUserService.java
+++ src/main/java/ru/cft/template/core/service/user/DefaultUserService.java
@@ -1,78 +1,64 @@
 package ru.cft.template.core.service.user;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.convert.ConversionService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import ru.cft.template.api.dto.UserDTO;
-import ru.cft.template.api.payload.user.NewUserPayload;
 import ru.cft.template.api.payload.user.PatchUserPayload;
+import ru.cft.template.api.payload.user.UserPayload;
 import ru.cft.template.core.entity.User;
-import ru.cft.template.core.exception.EntityNotFoundException;
+import ru.cft.template.core.entity.Wallet;
+import ru.cft.template.core.mapper.UserMapper;
 import ru.cft.template.core.repository.UserRepository;
-import ru.cft.template.core.utils.PasswordUtil;
+import ru.cft.template.core.repository.WalletRepository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @Validated
 @Service
 @RequiredArgsConstructor
 public class DefaultUserService implements UserService {
     private final UserRepository userRepository;
+    private final WalletRepository walletRepository;
 
-    private final ConversionService conversionService;
-    private final PasswordUtil passwordUtil;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
-    public UserDTO createUser(@Valid NewUserPayload userPayload) {
-        User user = this.conversionService.convert(userPayload, User.class);
-        user.setPasswordHash(this.passwordUtil.encode(user.getPasswordHash()));
-        return this.conversionService.convert(this.userRepository.save(user), UserDTO.class);
+    public UserDTO createUser(UserPayload userPayload) {
+        User savedUser = this.userRepository.save(userMapper.mapToUser(userPayload, passwordEncoder));
+        this.walletRepository.save(new Wallet(null, savedUser, 100L));
+        return userMapper.mapToUserDTO(savedUser);
     }
 
     @Override
     @Transactional
-    public UserDTO patchUser(Long id, @Valid PatchUserPayload userPayload) {
-        User user = this.userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("id", String.valueOf(id))));
-        if (Objects.nonNull(userPayload.firstName())) user.setFirstName(userPayload.firstName());
-        if (Objects.nonNull(userPayload.middleName())) user.setMiddleName(userPayload.middleName());
-        if (Objects.nonNull(userPayload.lastName())) user.setLastName(userPayload.lastName());
-        if (Objects.nonNull(userPayload.birthdate())) user.setBirthdate(LocalDate.parse(userPayload.birthdate(), DateTimeFormatter.ISO_DATE));
-        user.setLastUpdateDateTime(LocalDateTime.now());
-        return this.conversionService.convert(user, UserDTO.class);
+    public UserDTO patchUser(Long id, PatchUserPayload userPayload) {
+        User user = userRepository.findById(id).orElseThrow();
+        User patchedUser = userMapper.mapToUserPatch(user, userPayload);
+        return userMapper.mapToUserDTO(patchedUser);
     }
 
     @Override
     public List<UserDTO> findAllUsers() {
-        List<UserDTO> users = new ArrayList<>();
-        this.userRepository.findAll().forEach(user -> users.add(this.conversionService.convert(user, UserDTO.class)));
-        return users;
+        return userRepository.findAll().stream().map(userMapper::mapToUserDTO).toList();
     }
 
     @Override
     public UserDTO findUserById(Long id) {
-        User user = this.userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("id", String.valueOf(id))));
-        return this.conversionService.convert(user, UserDTO.class);
+        return userMapper.mapToUserDTO(userRepository.findById(id).orElseThrow());
     }
 
     @Override
     public UserDTO findUserByEmail(String email) {
-        User user = this.userRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("email", email)));
-        return this.conversionService.convert(user, UserDTO.class);
+        return userMapper.mapToUserDTO(userRepository.findByEmail(email).orElseThrow());
     }
 
     @Override
     public UserDTO findUserByPhoneNumber(String phoneNumber) {
-        User user = this.userRepository.findByPhoneNumber(phoneNumber).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("phoneNumber", phoneNumber)));
-        return this.conversionService.convert(user, UserDTO.class);
+        return userMapper.mapToUserDTO(userRepository.findByPhoneNumber(phoneNumber).orElseThrow());
     }
 }
--- src/main/java/ru/cft/template/core/service/wallet/DefaultWalletService.java
+++ src/main/java/ru/cft/template/core/service/wallet/DefaultWalletService.java
@@ -1,47 +1,31 @@
 package ru.cft.template.core.service.wallet;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.cft.template.api.dto.wallet.WalletDTO;
 import ru.cft.template.api.dto.wallet.WalletHesoyamDTO;
-import ru.cft.template.core.entity.User;
-import ru.cft.template.core.entity.Wallet;
-import ru.cft.template.core.exception.EntityNotFoundException;
-import ru.cft.template.core.repository.UserRepository;
+import ru.cft.template.core.mapper.WalletMapper;
 import ru.cft.template.core.repository.WalletRepository;
 import ru.cft.template.core.utils.HesoyamRouletteGeneratorUtil;
-
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultWalletService implements WalletService {
     private final WalletRepository walletRepository;
-    private final UserRepository userRepository;
 
-    private final ConversionService conversionService;
+    private final WalletMapper walletMapper;
     private final HesoyamRouletteGeneratorUtil hesoyamGenerator;
 
     @Override
     @Transactional
     public WalletDTO findByUserId(Long userId) {
-        Wallet wallet = this.walletRepository.findById(userId).orElseGet(() -> {
-            User user = this.userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(User.class.getSimpleName(), Map.of("id", String.valueOf(userId))));
-            return this.walletRepository.save(new Wallet(null, user, 100L));
-        });
-        return this.conversionService.convert(wallet, WalletDTO.class);
+        return walletMapper.mapToWalletDTO(walletRepository.findById(userId).orElseThrow());
     }
 
     @Override
     @Transactional
     public WalletHesoyamDTO hesoyam(Long userId) {
-        Wallet wallet = this.walletRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(Wallet.class.getSimpleName(), Map.of("id", String.valueOf(userId))));
-        if (this.hesoyamGenerator.call()) {
-            wallet.setHesoyamWinner(true);
-            wallet.setBalance(wallet.getBalance() + wallet.getHesoyamWinningAmount());
-        }
-        return this.conversionService.convert(wallet, WalletHesoyamDTO.class);
+        return walletMapper.mapToWalletHesoyam(walletRepository.findById(userId).orElseThrow(), this.hesoyamGenerator.call());
     }
 }
