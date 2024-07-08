1e84edfe79fb484ea6286f525c33c834ac306973
reyreavman <radmirkhurum@gmail.com>
2024-07-08 16:33:49 +0700

    - написал тесты для репозитория переводов

--- src/test/java/ru/cft/template/core/repository/TransferAmongUsersRepositoryIT.java
+++ src/test/java/ru/cft/template/core/repository/TransferAmongUsersRepositoryIT.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import ru.cft.template.core.entity.Wallet;
-import ru.cft.template.core.entity.transfer.TransferAmongUsers;
+import ru.cft.template.core.entity.transfer.Transfer;
 import ru.cft.template.core.entity.transfer.TransferStatus;
 import ru.cft.template.testContainers.PostgreSQLTestContainer;
 
@@ -22,7 +22,7 @@ public class TransferAmongUsersRepositoryIT {
     @ClassRule
     public static final PostgreSQLContainer postgres = PostgreSQLTestContainer.getInstance();
     @Autowired
-    public TransferAmongUsersRepository transferRepository;
+    public TransferRepository transferRepository;
     public List<Wallet> wallets = List.of(
             new Wallet(1L, null, 100L),
             new Wallet(2L, null, 100L),
@@ -30,78 +30,78 @@ public class TransferAmongUsersRepositoryIT {
             new Wallet(4L, null, 100L),
             new Wallet(5L, null, 100L)
     );
-    public List<TransferAmongUsers> expectedTransfers = List.of(
-            TransferAmongUsers.builder()
+    public List<Transfer> expectedTransfers = List.of(
+            Transfer.builder()
                     .id(1L)
                     .senderWallet(this.wallets.get(0))
                     .recipientWallet(this.wallets.get(1))
                     .amount(50L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(2L)
                     .senderWallet(this.wallets.get(2))
                     .recipientWallet(this.wallets.get(3))
                     .amount(20L)
                     .status(TransferStatus.FAILED)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(3L)
                     .senderWallet(this.wallets.get(1))
                     .recipientWallet(this.wallets.get(0))
                     .amount(75L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(4L)
                     .senderWallet(this.wallets.get(3))
                     .recipientWallet(this.wallets.get(4))
                     .amount(10L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(5L)
                     .senderWallet(this.wallets.get(4))
                     .recipientWallet(this.wallets.get(0))
                     .amount(30L)
                     .status(TransferStatus.FAILED)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(6L)
                     .senderWallet(this.wallets.get(2))
                     .recipientWallet(this.wallets.get(1))
                     .amount(60L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(7L)
                     .senderWallet(this.wallets.get(0))
                     .recipientWallet(this.wallets.get(3))
                     .amount(90L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(8L)
                     .senderWallet(this.wallets.get(4))
                     .recipientWallet(this.wallets.get(2))
                     .amount(15L)
                     .status(TransferStatus.FAILED)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(9L)
                     .senderWallet(this.wallets.get(1))
                     .recipientWallet(this.wallets.get(4))
                     .amount(85L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(10L)
                     .senderWallet(this.wallets.get(1))
                     .recipientWallet(this.wallets.get(4))
                     .amount(85L)
                     .status(TransferStatus.SUCCESSFUL)
                     .build(),
-            TransferAmongUsers.builder()
+            Transfer.builder()
                     .id(11L)
                     .senderWallet(this.wallets.get(3))
                     .recipientWallet(this.wallets.get(0))
@@ -113,9 +113,9 @@ public class TransferAmongUsersRepositoryIT {
     @Test
     void findAllByStatus_ReturnsAllTransfersWithStatus() {
         TransferStatus status = TransferStatus.SUCCESSFUL;
-        List<TransferAmongUsers> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getStatus().equals(status)).toList();
+        List<Transfer> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getStatus().equals(status)).toList();
 
-        List<TransferAmongUsers> actualTransfers = this.transferRepository.findAllByStatus(status);
+        List<Transfer> actualTransfers = this.transferRepository.findAllByStatus(status);
 
         Assertions.assertThat(actualTransfers)
                 .usingRecursiveComparison()
@@ -126,9 +126,9 @@ public class TransferAmongUsersRepositoryIT {
     @Test
     void findAllBySenderWalletId_ReturnsAllTransfersFoundWithSenderWalletId() {
         Long walletId = 4L;
-        List<TransferAmongUsers> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getSenderWallet().getId().equals(walletId)).toList();
+        List<Transfer> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getSenderWallet().getId().equals(walletId)).toList();
 
-        List<TransferAmongUsers> actualTransfers = this.transferRepository.findAllBySenderWalletId(walletId);
+        List<Transfer> actualTransfers = this.transferRepository.findAllBySenderWalletId(walletId);
 
         Assertions.assertThat(actualTransfers)
                 .usingRecursiveComparison()
@@ -139,9 +139,9 @@ public class TransferAmongUsersRepositoryIT {
     @Test
     void findAllByRecipientWalletId_ReturnsAllTransfersFoundWithRecipientWalletId() {
         Long walletId = 5L;
-        List<TransferAmongUsers> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId)).toList();
+        List<Transfer> expectedTransfers = this.expectedTransfers.stream().filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId)).toList();
 
-        List<TransferAmongUsers> actualTransfers = this.transferRepository.findAllByRecipientWalletId(walletId);
+        List<Transfer> actualTransfers = this.transferRepository.findAllByRecipientWalletId(walletId);
 
         Assertions.assertThat(actualTransfers)
                 .usingRecursiveComparison()
