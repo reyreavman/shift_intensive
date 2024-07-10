737fe359d01fdde4dd11fe1d75e82e9d8b3e8030
reyreavman <radmirkhurum@gmail.com>
2024-07-10 16:52:10 +0700

    - написал тесты для transfer репозитория

---
+++ src/test/java/ru/cft/template/core/repository/TransferRepositoryIT.java
@@ -0,0 +1,196 @@
+package ru.cft.template.core.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import ru.cft.template.core.entity.Wallet;
+import ru.cft.template.core.entity.transfer.Transfer;
+import ru.cft.template.core.entity.transfer.TransferStatus;
+import ru.cft.template.testContainers.PostgreSQLTestContainer;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Sql("/sql/V1_1_1__Basic_schema.sql")
+public class TransferRepositoryIT {
+    @ClassRule
+    public static final PostgreSQLContainer postgres = PostgreSQLTestContainer.getInstance();
+    @Autowired
+    public TransferRepository transferRepository;
+    public List<Wallet> preparedWallets = WalletRepositoryIT.preparedWallets;
+    public List<Transfer> preparedTransfers = List.of(
+            Transfer.builder()
+                    .id(1L)
+                    .senderWallet(this.preparedWallets.get(0))
+                    .recipientWallet(this.preparedWallets.get(1))
+                    .amount(50L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(2L)
+                    .senderWallet(this.preparedWallets.get(2))
+                    .recipientWallet(this.preparedWallets.get(3))
+                    .amount(20L)
+                    .status(TransferStatus.FAILED)
+                    .build(),
+            Transfer.builder()
+                    .id(3L)
+                    .senderWallet(this.preparedWallets.get(1))
+                    .recipientWallet(this.preparedWallets.get(0))
+                    .amount(75L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(4L)
+                    .senderWallet(this.preparedWallets.get(3))
+                    .recipientWallet(this.preparedWallets.get(4))
+                    .amount(10L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(5L)
+                    .senderWallet(this.preparedWallets.get(4))
+                    .recipientWallet(this.preparedWallets.get(0))
+                    .amount(30L)
+                    .status(TransferStatus.FAILED)
+                    .build(),
+            Transfer.builder()
+                    .id(6L)
+                    .senderWallet(this.preparedWallets.get(2))
+                    .recipientWallet(this.preparedWallets.get(1))
+                    .amount(60L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(7L)
+                    .senderWallet(this.preparedWallets.get(0))
+                    .recipientWallet(this.preparedWallets.get(3))
+                    .amount(90L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(8L)
+                    .senderWallet(this.preparedWallets.get(4))
+                    .recipientWallet(this.preparedWallets.get(2))
+                    .amount(15L)
+                    .status(TransferStatus.FAILED)
+                    .build(),
+            Transfer.builder()
+                    .id(9L)
+                    .senderWallet(this.preparedWallets.get(1))
+                    .recipientWallet(this.preparedWallets.get(4))
+                    .amount(85L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(10L)
+                    .senderWallet(this.preparedWallets.get(1))
+                    .recipientWallet(this.preparedWallets.get(4))
+                    .amount(85L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build(),
+            Transfer.builder()
+                    .id(11L)
+                    .senderWallet(this.preparedWallets.get(3))
+                    .recipientWallet(this.preparedWallets.get(0))
+                    .amount(25L)
+                    .status(TransferStatus.SUCCESSFUL)
+                    .build());
+
+    private void assertions(List<Transfer> actualTransfers, List<Transfer> expectedTransfers) {
+        Assertions.assertThat(actualTransfers)
+                .usingRecursiveComparison()
+                .ignoringFields("senderWallet", "recipientWallet")
+                .isEqualTo(expectedTransfers);
+    }
+
+    @Test
+    void findAllBySenderWalletId_ReturnsAllTransferWithSenderWalletId() {
+        Long senderWalletId = 3L;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getSenderWallet().getId().equals(senderWalletId))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllBySenderWalletId(senderWalletId);
+
+        assertions(actualTransfers, expectedTransfers);
+    }
+
+    @Test
+    void findAllBySenderWalletIdAndStatus_ReturnsAllTransferWithSenderWalletIdAndStatus() {
+        Long senderWalletId = 5L;
+        TransferStatus transferStatus = TransferStatus.FAILED;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getSenderWallet().getId().equals(senderWalletId) && transfer.getStatus().equals(transferStatus))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllBySenderWalletIdAndStatus(senderWalletId, transferStatus);
+
+        assertions(actualTransfers, expectedTransfers);
+    }
+
+    @Test
+    void findAllByRecipientWalletId_ReturnsAllTransfersFoundWithRecipientWalletId() {
+        Long walletId = 5L;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllByRecipientWalletId(walletId);
+
+        assertions(actualTransfers, expectedTransfers);
+    }
+
+    @Test
+    void findAllByRecipientWalletIdAndStatus_ReturnsAllTransfersWithRecipientWalletIdAndStatus() {
+        Long walletId = 1L;
+        TransferStatus status = TransferStatus.SUCCESSFUL;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId) && transfer.getStatus().equals(status))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllByRecipientWalletIdAndStatus(walletId, status);
+
+        assertions(actualTransfers, expectedTransfers);
+    }
+
+    @Test
+    void findAllBySenderWalletIdOrRecipientWalletId_ReturnsAllTransfersWithSenderWalletIdOrRecipientWalletId() {
+        Long walletId = 4L;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId) || transfer.getSenderWallet().getId().equals(walletId))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllBySenderWalletIdOrRecipientWalletId(walletId, walletId);
+
+        assertions(actualTransfers, expectedTransfers);
+    }
+
+    @Test
+    void findAllBySenderWalletIdOrRecipientWalletIdAndStatus_ReturnsAllTransfersWithSenderWalletIdOrRecipientWalletId() {
+        Long walletId = 2L;
+        TransferStatus status = TransferStatus.SUCCESSFUL;
+        List<Transfer> expectedTransfers = preparedTransfers
+                .stream()
+                .filter(transfer -> transfer.getRecipientWallet().getId().equals(walletId) || transfer.getSenderWallet().getId().equals(walletId))
+                .filter(transfer -> transfer.getStatus().equals(status))
+                .toList();
+
+        List<Transfer> actualTransfers = transferRepository.findAllBySenderWalletIdOrRecipientWalletIdAndStatus(walletId, walletId, status);
+
+        assertions(actualTransfers, expectedTransfers);
+        Assertions.assertThat(actualTransfers)
+                .hasSize(5);
+    }
+}
