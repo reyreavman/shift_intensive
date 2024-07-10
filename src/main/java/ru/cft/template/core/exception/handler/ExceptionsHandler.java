9331b5c2c7b427ce19f11f69d45da70d8bf2cf6f
reyreavman <radmirkhurum@gmail.com>
2024-07-10 14:37:06 +0700

    - зарефачил инвойс сервер, выглядит теперь интереснее - в exceptionHandler добавлена обработка DataIntegrityViolationException

--- src/main/java/ru/cft/template/core/exception/handler/ExceptionsHandler.java
+++ src/main/java/ru/cft/template/core/exception/handler/ExceptionsHandler.java
@@ -1,5 +1,7 @@
 package ru.cft.template.core.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,22 +12,33 @@ import ru.cft.template.core.exception.service.ServiceException;
 import ru.cft.template.core.exception.validation.ValidationErrorResponse;
 import ru.cft.template.core.exception.validation.Violation;
 
+import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionsHandler {
     @ExceptionHandler
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ServiceErrorResponse handleServiceException(ServiceException e) {
-        return new ServiceErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+        return new ServiceErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+        List<Violation> violations = e.getBindingResult().getFieldErrors()
+                .stream()
                 .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
                 .toList();
         return new ValidationErrorResponse(violations);
     }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ServiceErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        List<String> keys = Arrays.stream(e.getMostSpecificCause().getMessage().split(":")).toList();
+        return new ServiceErrorResponse(keys.get(keys.size() - 1).trim());
+    }
+
 }
--- src/main/java/ru/cft/template/core/service/invoice/DefaultInvoiceService.java
+++ src/main/java/ru/cft/template/core/service/invoice/DefaultInvoiceService.java
@@ -1,7 +1,6 @@
 package ru.cft.template.core.service.invoice;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.cft.template.api.dto.invoice.CreateInvoiceDTO;
@@ -10,75 +9,59 @@ import ru.cft.template.api.dto.invoice.InvoiceTotalDTO;
 import ru.cft.template.api.dto.invoice.PayInvoiceDTO;
 import ru.cft.template.api.dto.invoice.common.InvoiceDirectionType;
 import ru.cft.template.api.dto.invoice.common.InvoiceFilters;
+import ru.cft.template.api.dto.invoice.common.InvoiceStatus;
 import ru.cft.template.core.entity.User;
-import ru.cft.template.core.entity.Wallet;
 import ru.cft.template.core.entity.invoice.Invoice;
-import ru.cft.template.core.entity.invoice.InvoiceStatus;
+import ru.cft.template.core.entity.transfer.Transfer;
 import ru.cft.template.core.entity.transfer.TransferStatus;
+import ru.cft.template.core.exception.service.ServiceException;
 import ru.cft.template.core.mapper.InvoiceMapper;
-import ru.cft.template.core.mapper.TransferMapper;
 import ru.cft.template.core.repository.InvoiceRepository;
-import ru.cft.template.core.repository.TransferRepository;
-import ru.cft.template.core.repository.UserRepository;
-import ru.cft.template.core.repository.WalletRepository;
+import ru.cft.template.core.service.transfer.TransferService;
+import ru.cft.template.core.service.user.UserService;
 
 import java.time.chrono.ChronoLocalDateTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultInvoiceService implements InvoiceService {
     private final InvoiceRepository invoiceRepository;
-    private final UserRepository userRepository;
-    private final TransferRepository transferRepository;
-    private final WalletRepository walletRepository;
+
+    private final UserService userService;
+    private final TransferService transferService;
 
     private final InvoiceMapper invoiceMapper;
-    private final TransferMapper transferMapper;
 
 
     @Override
     public InvoiceDataDTO createInvoice(Long userId, CreateInvoiceDTO createInvoiceDTO) {
-        User sender = userRepository.findById(userId).orElseThrow();
-        User recipient = userRepository.findById(createInvoiceDTO.recipientId()).orElseThrow();
-        Invoice savedInvoice = invoiceRepository.save(invoiceMapper.mapToInvoice(sender, recipient, createInvoiceDTO.amount(), createInvoiceDTO.comment()));
+        User sender = userService.findUserByIdEntity(userId);
+        User recipient = userService.findUserByIdEntity(createInvoiceDTO.recipientId());
+        Invoice savedInvoice = invoiceRepository.save(invoiceMapper.mapToInvoice(sender, recipient, createInvoiceDTO));
         return invoiceMapper.mapToInvoiceDataDTO(savedInvoice);
     }
 
     @Override
     @Transactional
-    public InvoiceDataDTO payInvoice(Long userId, PayInvoiceDTO invoicePayload) {
-        Invoice invoice = invoiceRepository.findById(invoicePayload.invoiceId()).orElseThrow();
-        User recipient = userRepository.findById(userId).orElseThrow();
+    public InvoiceDataDTO payInvoice(Long id, PayInvoiceDTO invoicePayload) {
+        Invoice invoice = findInvoiceById(invoicePayload.invoiceId());
 
-        if (invoice.getStatus().equals(InvoiceStatus.PAID))
-            throw new ServiceException("Cчет - %s уже оплачен".formatted(invoice.getId()));
-        if (!invoice.getRecipient().equals(recipient))
-            throw new ServiceException("Пользователь - %d не может оплатить данный счет.".formatted(userId));
-        if (!Objects.equals(invoicePayload.amount(), invoice.getAmount()))
-            throw new ServiceException("Попытка оплатить не полную сумму услуги.");
+        validateInvoiceToPay(invoice, invoice.getRecipient());
+        Transfer transfer = transferService.createTransfer(invoice.getRecipient().getId(), invoice.getSender().getId(), invoice.getAmount());
 
-        Wallet recipientWallet = walletRepository.findById(userId).orElseThrow();
-        Wallet senderWallet = walletRepository.findById(invoice.getSender().getId()).orElseThrow();
-        if (recipientWallet.getBalance() >= invoice.getAmount()) {
-            recipientWallet.setBalance(recipientWallet.getBalance() - invoice.getAmount());
-            senderWallet.setBalance(senderWallet.getBalance() + invoice.getAmount());
-            invoice.setStatus(InvoiceStatus.PAID);
-            transferRepository.save(transferMapper.mapToTransfer(invoice.getAmount(), recipientWallet, senderWallet, TransferStatus.SUCCESSFUL));
-            return invoiceMapper.mapToInvoiceDataDTO(invoice);
-        } else {
-            transferRepository.save(transferMapper.mapToTransfer(invoice.getAmount(), recipientWallet, senderWallet, TransferStatus.FAILED));
-            throw new ServiceException("На счету недостаточно средств.");
-        }
+        if (transfer.getStatus().equals(TransferStatus.SUCCESSFUL)) invoice.setStatus(InvoiceStatus.PAID);
+
+        return invoiceMapper.mapToInvoiceDataDTO(invoice);
     }
 
     @Override
     @Transactional
     public InvoiceDataDTO cancelInvoice(Long userId, UUID invoiceId) {
-        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow();
+        Invoice invoice = findInvoiceById(invoiceId);
         if (!Objects.equals(invoice.getSender().getId(), userId))
             throw new ServiceException("Пользователь - %d не может отменить данный счет.");
         invoice.setStatus(InvoiceStatus.CANCELED);
@@ -87,72 +70,59 @@ public class DefaultInvoiceService implements InvoiceService {
 
     @Override
     public InvoiceDataDTO getInvoiceDataById(UUID invoiceUUID) {
-        return invoiceMapper.mapToInvoiceDataDTO(invoiceRepository.findById(invoiceUUID).orElseThrow());
+        return invoiceMapper.mapToInvoiceDataDTO(findInvoiceById(invoiceUUID));
     }
 
     @Override
     public List<InvoiceDataDTO> getUserInvoicesWithFilters(Long userId, InvoiceFilters filters) {
-        List<Predicate<Invoice>> predicates = invoiceFiltersPredicates(filters);
         if (filters.getDirectionType().equals(InvoiceDirectionType.INCOMING))
-            return invoiceRepository.findAllByRecipientId(userId).stream()
-                    .filter(predicates.get(0))
-                    .filter(predicates.get(1))
-                    .filter(predicates.get(2))
+            return invoicesFiltersChain(invoiceRepository.findAllByRecipientId(userId).stream(), filters)
                     .map(invoiceMapper::mapToInvoiceDataDTO)
                     .toList();
-        return invoiceRepository.findAllBySenderId(userId).stream()
-                .filter(predicates.get(0))
-                .filter(predicates.get(1))
-                .filter(predicates.get(2))
+        return invoicesFiltersChain(invoiceRepository.findAllBySenderId(userId).stream(), filters)
                 .map(invoiceMapper::mapToInvoiceDataDTO)
                 .toList();
     }
 
     @Override
     public InvoiceTotalDTO getUserInvoicesTotalWithFilters(Long userId, InvoiceFilters filters) {
-        List<Predicate<Invoice>> predicates = invoiceFiltersPredicates(filters);
-
         if (filters.getDirectionType().equals(InvoiceDirectionType.INCOMING)) {
-            long totalAmount = invoiceRepository.findAllByRecipientId(userId).stream()
-                    .filter(predicates.get(0))
-                    .filter(predicates.get(1))
-                    .filter(predicates.get(2))
+            long totalAmount = invoicesFiltersChain(invoiceRepository.findAllByRecipientId(userId).stream(), filters)
                     .mapToLong(Invoice::getAmount).sum();
             return invoiceMapper.mapToInvoiceTotalDTO(userId, filters, totalAmount);
         }
-        long totalAmount = invoiceRepository.findAllByRecipientId(userId).stream()
-                .filter(predicates.get(0))
-                .filter(predicates.get(1))
-                .filter(predicates.get(2))
+        long totalAmount = invoicesFiltersChain(invoiceRepository.findAllByRecipientId(userId).stream(), filters)
                 .mapToLong(Invoice::getAmount).sum();
         return invoiceMapper.mapToInvoiceTotalDTO(userId, filters, totalAmount);
     }
 
     @Override
     public InvoiceDataDTO getOldestUserInvoice(Long userId, InvoiceFilters filters) {
-        List<Predicate<Invoice>> predicates = invoiceFiltersPredicates(filters);
-
         if (filters.getDirectionType().equals(InvoiceDirectionType.INCOMING)) {
-            Invoice oldestUserInvoice = invoiceRepository.findByRecipientIdOrderByCreationDateTimeAsc(userId).stream()
-                    .filter(predicates.get(0))
-                    .filter(predicates.get(1))
-                    .filter(predicates.get(2))
+            Invoice oldestUserInvoice = invoicesFiltersChain(invoiceRepository.findByRecipientIdOrderByCreationDateTimeAsc(userId).stream(), filters)
                     .findFirst().orElseThrow();
             return invoiceMapper.mapToInvoiceDataDTO(oldestUserInvoice);
         }
-        Invoice oldestUserInvoice = invoiceRepository.findBySenderIdOrderByCreationDateTimeAsc(userId).stream()
-                .filter(predicates.get(0))
-                .filter(predicates.get(1))
-                .filter(predicates.get(2))
+        Invoice oldestUserInvoice = invoicesFiltersChain(invoiceRepository.findBySenderIdOrderByCreationDateTimeAsc(userId).stream(), filters)
                 .findFirst().orElseThrow();
         return invoiceMapper.mapToInvoiceDataDTO(oldestUserInvoice);
     }
 
-    private List<Predicate<Invoice>> invoiceFiltersPredicates(InvoiceFilters filters) {
-        return List.of(
-                invoice -> Objects.isNull(filters.getStatus()) || invoice.getStatus().equals(filters.getStatus()),
-                invoice -> Objects.isNull(filters.getStart()) || invoice.getCreationDateTime().isAfter(ChronoLocalDateTime.from(filters.getStart())),
-                invoice -> Objects.isNull(filters.getEnd()) || invoice.getCreationDateTime().isBefore(ChronoLocalDateTime.from(filters.getEnd()))
-        );
+    private Stream<Invoice> invoicesFiltersChain(Stream<Invoice> invoicesStream, InvoiceFilters filters) {
+        return invoicesStream
+                .filter(invoice -> Objects.isNull(filters.getStatus()) || invoice.getStatus().equals(filters.getStatus()))
+                .filter(invoice -> Objects.isNull(filters.getStart()) || invoice.getCreationDateTime().isAfter(ChronoLocalDateTime.from(filters.getStart())))
+                .filter(invoice -> Objects.isNull(filters.getEnd()) || invoice.getCreationDateTime().isBefore(ChronoLocalDateTime.from(filters.getEnd())));
+    }
+
+    private void validateInvoiceToPay(Invoice invoice, User recipient) {
+        if (invoice.getStatus().equals(InvoiceStatus.PAID))
+            throw new ServiceException("Cчет - %s уже оплачен".formatted(invoice.getId()));
+        if (!invoice.getRecipient().equals(recipient))
+            throw new ServiceException("Пользователь - %d не может оплатить данный счет.".formatted(recipient.getId()));
+    }
+
+    private Invoice findInvoiceById(UUID invoiceId) {
+        return invoiceRepository.findById(invoiceId).orElseThrow(() -> new ServiceException("Счет с id - %s не найден".formatted(invoiceId.toString())));
     }
 }
