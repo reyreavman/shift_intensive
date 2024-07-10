714565ab71ae3f5c1db142068456615eec735950
reyreavman <radmirkhurum@gmail.com>
2024-07-10 15:24:03 +0700

    - написаны интеграционные тесты для wallet репозитория

---
+++ src/test/java/ru/cft/template/core/repository/WalletRepositoryIT.java
@@ -0,0 +1,62 @@
+package ru.cft.template.core.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import ru.cft.template.core.entity.User;
+import ru.cft.template.core.entity.Wallet;
+import ru.cft.template.testContainers.PostgreSQLTestContainer;
+
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@Transactional
+@Sql("/sql/V1_1_1__Basic_schema.sql")
+public class WalletRepositoryIT {
+    @ClassRule
+    public static final PostgreSQLContainer postgres = PostgreSQLTestContainer.getInstance();
+    @Autowired
+    public WalletRepository walletRepository;
+    public final List<User> preparedUsers = List.of(
+            User.builder().phoneNumber("79123456789").email("ivanov@example.com").build(),
+            User.builder().phoneNumber("79876543210").email("sidoorova@example.com").build(),
+            User.builder().phoneNumber("79032145678").email("kuznetsov@example.com").build(),
+            User.builder().phoneNumber("79243256789").email("popova@example.com").build(),
+            User.builder().phoneNumber("79456372891").email("divanov@example.com").build()
+    );
+    public List<Wallet> preparedWallets = List.of(
+            new Wallet(1L, preparedUsers.get(0), 100L),
+            new Wallet(2L, preparedUsers.get(1), 100L),
+            new Wallet(3L, preparedUsers.get(2), 100L),
+            new Wallet(4L, preparedUsers.get(3), 100L),
+            new Wallet(5L, preparedUsers.get(4), 100L)
+    );
+
+    @Test
+    void findByUserPhoneNumber_ReturnsUserFoundByPhoneNumber() {
+        String phoneNumber = "79123456789";
+        Wallet expectedWallet = Optional.of(preparedWallets.get(0)).get();
+
+        Wallet actualWallet = walletRepository.findByUserPhoneNumber(phoneNumber).get();
+
+        Assertions.assertThat(actualWallet.getUser().getPhoneNumber()).isEqualTo(expectedWallet.getUser().getPhoneNumber());
+        Assertions.assertThat(actualWallet.getUser().getEmail()).isEqualTo(expectedWallet.getUser().getEmail());
+    }
+
+    @Test
+    void findByUserEmail_ReturnsUserFoundByEmail() {
+        String email = "popova@example.com";
+        Wallet expectedWallet = Optional.of(preparedWallets.get(3)).get();
+
+        Wallet actualWallet = walletRepository.findByUserEmail(email).get();
+
+        Assertions.assertThat(actualWallet.getUser().getPhoneNumber()).isEqualTo(expectedWallet.getUser().getPhoneNumber());
+        Assertions.assertThat(actualWallet.getUser().getEmail()).isEqualTo(expectedWallet.getUser().getEmail());
+    }
+}
--- src/test/resources/sql/V1_1_1__Basic_schema.sql
+++ src/test/resources/sql/V1_1_1__Basic_schema.sql
@@ -3,19 +3,19 @@ create schema if not exists wallet;
 drop table if exists wallet.users;
 
 create table if not exists wallet.users (
-    id                              serial primary key,
-    first_name                    varchar(50) not null check (c_first_name ~* '^[А-ЯЁ][а-яё]{1,50}$'),
-    middle_name                   varchar(50) default null check (c_middle_name ~* '^[А-ЯЁ][а-яё]{1,50}$'),
-    last_name                     varchar(50) not null check (c_last_name ~* '^[А-ЯЁ][а-яё]{1,50}$'),
-    phone_number                  varchar(11) not null unique check (c_phone_number ~* '^7\d{10}$'),
-    email                         varchar(255) not null unique check (c_email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+    id                            serial primary key,
+    first_name                    varchar(50) not null,
+    middle_name                   varchar(50),
+    last_name                     varchar(50) not null,
+    phone_number                  varchar(11) not null unique,
+    email                         varchar(255) not null unique,
     birthdate                     date not null,
     password_hash                 varchar(255) not null,
-    registration_date_time        timestamp default null,
-    last_update_date_time         timestamp default null
+    creation_time_stamp           timestamp,
+    last_update_time_stamp        timestamp
 );
 
-insert into wallet.users (c_first_name, c_middle_name, c_last_name, c_phone_number, c_email, c_birthdate, c_password_hash) values
+insert into wallet.users (first_name, middle_name, last_name, phone_number, email, birthdate, password_hash) values
     ('Иван', 'Иванович', 'Петров', '79123456789', 'ivanov@example.com', '1980-01-01', 'hashed_password'),
     ('Мария', 'Петровна', 'Сидорова', '79876543210', 'sidoorova@example.com', '1990-02-02', 'another_hashed_password'),
     ('Александр', 'Васильевич', 'Кузнецов', '79032145678', 'kuznetsov@example.com', '2000-03-03', 'third_hashed_password'),
@@ -24,28 +24,28 @@ insert into wallet.users (c_first_name, c_middle_name, c_last_name, c_phone_numb
 
 
 create table if not exists wallet.wallets (
-    user_id                int primary key references wallet.users(id),
-    c_balance         int not null default 100
+    user_id           int primary key references wallet.users(id),
+    balance           int not null
 );
 
-insert into wallet.wallets (user_id) values
-    (1),
-    (2),
-    (3),
-    (4),
-    (5);
+insert into wallet.wallets (user_id, balance) values
+    (1, 100),
+    (2, 100),
+    (3, 100),
+    (4, 100),
+    (5, 100);
 
 
 create table if not exists wallet.transfers_among_users (
     id                  serial primary key,
-    sender_id          int not null references wallet.users(id),
-    recipient_id       int not null references wallet.users(id),
-    amount            int not null check (c_amount > 0),
-    status            varchar not null,
-    date_time          timestamp default null
+    sender_id           int not null references wallet.users(id),
+    recipient_id        int not null references wallet.users(id),
+    amount              int not null,
+    status              varchar not null,
+    date_time           timestamp
 );
 
-insert into wallet.transfers_among_users (c_sender_id, c_recipient_id, c_amount, c_status) values
+insert into wallet.transfers_among_users (sender_id, recipient_id, amount, status) values
     (1, 2, 50, 'SUCCESSFUL'),
     (3, 4, 20, 'FAILED'),
     (2, 1, 75, 'SUCCESSFUL'),
