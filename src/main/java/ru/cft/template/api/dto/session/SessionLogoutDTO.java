4c53127320a94eef8638415c78f42d10e88719be
reyreavman <radmirkhurum@gmail.com>
2024-07-10 14:29:45 +0700

    - добавлен @компонент к генератору

--- src/main/java/ru/cft/template/api/dto/session/SessionLogoutDTO.java
+++
@@ -1,9 +0,0 @@
-package ru.cft.template.api.dto.session;
-
-import ru.cft.template.common.SessionsStatus;
-
-public record SessionLogoutDTO(
-        Long id,
-        SessionsStatus status
-) {
-}
--- src/main/java/ru/cft/template/core/utils/HesoyamRouletteGeneratorUtil.java
+++ src/main/java/ru/cft/template/core/utils/HesoyamRouletteGeneratorUtil.java
@@ -1,14 +1,18 @@
 package ru.cft.template.core.utils;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.cft.template.api.dto.wallet.HesoyamResult;
 
 import java.util.Random;
 
+@Component
 @RequiredArgsConstructor
 public class HesoyamRouletteGeneratorUtil {
-    private final Random random;
-    private final int chance;
+    private final Random random = new Random();
+    @Getter
+    private final int chance = 25;
 
     public HesoyamResult call() {
         return random.nextInt(1, 101) <= chance ? HesoyamResult.WINNER : HesoyamResult.LOSER;
