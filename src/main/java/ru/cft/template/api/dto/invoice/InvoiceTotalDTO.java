ba8015bc5bdb1a1113578f77b95c111c39516c91
reyreavman <radmirkhurum@gmail.com>
2024-07-11 07:42:01 +0700

    - из invoiceTotalDto убраны фильтры - в проперти добавлены месседжи для валидации

--- src/main/java/ru/cft/template/api/dto/invoice/InvoiceTotalDTO.java
+++ src/main/java/ru/cft/template/api/dto/invoice/InvoiceTotalDTO.java
@@ -1,12 +1,10 @@
 package ru.cft.template.api.dto.invoice;
 
 import lombok.Builder;
-import ru.cft.template.api.dto.invoice.common.InvoiceFilters;
 
 @Builder
 public record InvoiceTotalDTO(
         Long userId,
-        InvoiceFilters filters,
         Long total
 ) {
 }
