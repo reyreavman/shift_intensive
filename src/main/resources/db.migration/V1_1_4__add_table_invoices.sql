cd500aa3df504b8fea42ed8e29f96693c68c4602
reyreavman <radmirkhurum@gmail.com>
2024-07-11 07:39:34 +0700

    - добавлена миграция

--- src/main/resources/db.migration/V1_1_4__add_table_invoices.sql
+++ src/main/resources/db.migration/V1_1_4__add_table_invoices.sql
@@ -5,7 +5,7 @@ create table if not exists wallet.invoices (
     sender_id           int not null references wallet.users(id),
     recipient_id        int not null references wallet.users(id),
     amount              int not null,
-    transfer_id         int references wallet.transfers(id)
+    transfer_id         int references wallet.transfers(id),
     comment             varchar(250),
     status              varchar not null,
     creation_date_time  timestamp not null
